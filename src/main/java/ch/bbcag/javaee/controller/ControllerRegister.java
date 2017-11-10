93c6455feac9cb1571c2ab8e31368649991be6b2
Benjamin Raison <benjaminraison@rocketmail.com>
2017-11-10 15:20:07 +0100

    Tried to get URL rewriting to work. tl;dr: it doesn't work

--- src/main/java/ch/bbcag/javaee/controller/ControllerRegister.java
+++ src/main/java/ch/bbcag/javaee/controller/ControllerRegister.java
@@ -1,4 +1,18 @@
 package ch.bbcag.javaee.controller;
 
-public class ControllerRegister {
+import com.ocpsoft.pretty.faces.annotation.URLMapping;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.ManagedBean;
+import java.io.Serializable;
+
+@ManagedBean
+@SessionScoped
+@URLMapping(id = "register", pattern = "/example/register/", viewId = "/example/register.jsf")
+public class ControllerRegister implements Serializable {
+
+
+    public String getText() {
+        return "Hello World";
+    }
 }
