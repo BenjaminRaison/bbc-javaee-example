e9f4c0d4653796bb650009794110567b899d123c
Benjamin Raison <benjaminraison@rocketmail.com>
2017-11-15 18:12:07 +0100

    BROKEN! (Maybe). Gave up on url rewriting,

---
+++ src/main/java/ch/bbcag/javaee/controller/ControllerIndex.java
@@ -0,0 +1,10 @@
+package ch.bbcag.javaee.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@RequestScoped
+@Named
+public class ControllerIndex implements Serializable {
+}
--- src/main/java/ch/bbcag/javaee/controller/ControllerRegister.java
+++ src/main/java/ch/bbcag/javaee/controller/ControllerRegister.java
@@ -1,41 +1,22 @@
 package ch.bbcag.javaee.controller;
 
-import ch.bbcag.javaee.model.Item;
-import com.ocpsoft.pretty.faces.annotation.URLMapping;
-
-import javax.annotation.Resource;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
-import javax.faces.bean.ManagedBean;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import javax.transaction.*;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
 import java.io.Serializable;
 
-@ManagedBean
 @SessionScoped
-@URLMapping(id = "register", pattern = "/register/", viewId = "/register.jsf")
+@Named
 public class ControllerRegister implements Serializable {
 
-    @PersistenceUnit
-    private EntityManagerFactory emf;
-
-    @Resource
-    private UserTransaction ut;
-
     public String getText() {
-        try {
-            ut.begin();
-            EntityManager manager = emf.createEntityManager();
-            Item item = new Item();
-            item.setTitle("Hi");
-            item.setPrice(25d);
-            item.setDescription("Blablabla");
-            manager.persist(item);
-            ut.commit();
-        } catch (RollbackException | HeuristicMixedException | SystemException | HeuristicRollbackException | NotSupportedException e) {
-            e.printStackTrace();
-        }
         return "Hello World";
     }
+
+    @PostConstruct
+    public void postConstruct() {
+        FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+    }
+
 }
